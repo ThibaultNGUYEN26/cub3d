8b02b06eb797be0677347ebbca8a1ca9b1e285e4
riri666 <chbouki.riham@gmail.com>
2024-01-25 20:31:49 +0100

    check_parsing_2.c norm OK

--- cub3d/src/check_parsing_2.c
+++ cub3d/src/check_parsing_2.c
@@ -6,7 +6,7 @@
 /*   By: rchbouki <rchbouki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 00:41:17 by rchbouki          #+#    #+#             */
-/*   Updated: 2024/01/25 20:22:30 by rchbouki         ###   ########.fr       */
+/*   Updated: 2024/01/25 20:31:17 by rchbouki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int	end_parsing(int *k, int start, t_var *var)
 {
 	int	i;
-	
+
 	*k = start;
 	i = -1;
 	while (++i < 6)
@@ -41,7 +41,7 @@ void	ft_affect_color(t_data *data, char *cmp, t_color color)
 {
 	unsigned int	color_hexa;
 	char			*str;
-	
+
 	color_hexa = ft_convert_color(color);
 	str = ft_itoa(color_hexa);
 	element_affect(data, cmp, str, 1);
